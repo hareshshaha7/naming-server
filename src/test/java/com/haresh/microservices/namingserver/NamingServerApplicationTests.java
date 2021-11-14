413206e24df4b218a47dee353c1ff42cc0086863
Haresh Shaha <hudaysha@tibco.com>
2021-11-14 14:23:44 +0530

    Initial commit

---
+++ src/test/java/com/haresh/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haresh.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
